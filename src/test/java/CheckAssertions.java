7aed19d76d0b4e5c60187d1d226a01947b215e0d
Kateryna Kuzenkova <kompanenko96@gmail.com>
2020-02-20 23:06:28 +0200

    Updated Asserts

--- src/test/java/CheckAssertions.java
+++ src/test/java/CheckAssertions.java
@@ -1,7 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
 
 public class CheckAssertions {
 
@@ -10,12 +13,14 @@ public class CheckAssertions {
         System.setProperty("webdriver.chrome.driver", "/Users/katerynakuzenkova/Desktop/Drivers/chromedriver");
         WebDriver chromeDriver = new ChromeDriver();
         chromeDriver.get("https://www.youtube.com/");
-        Assert.assertEquals("Youtube", chromeDriver.getTitle());
-        Assert.assertEquals("Youtube", chromeDriver.getTitle(), "Strings are not matching" );
-        Assert.assertEquals("Youtube",chromeDriver.getTitle());
-        Assert.assertEquals("Youtube", chromeDriver.getTitle());
-        Assert.assertTrue(true);
 
+        Assert.assertEquals(chromeDriver.getTitle(), "YouTube");
+        Assert.assertEquals(chromeDriver.getTitle(), "YouTube", "Strings are not matching" );
+        Assert.assertEquals(chromeDriver.getTitle().startsWith("You"), true);
+        Assert.assertTrue(chromeDriver.getTitle().endsWith("ube"));
+        Assert.assertTrue(chromeDriver.getTitle().endsWith("Tube"), "Strings are not matching");
+        Assert.assertFalse(chromeDriver.getTitle().startsWith("oi"));
+        Assert.assertFalse(chromeDriver.getTitle().endsWith("de"), "AssertFalse test failed");
     }
 }
 
